af0d644f42a8961b5b17d8b2c317e8343ed7fdcc
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2025-12-27 09:36:02 +0530

    finished structure

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = ({ children }) => {
+    return (
+        <div className='max-w-[85rem] m-auto'>
+            {children}
+        </div>
+    )
+}
+
+export default Layout
