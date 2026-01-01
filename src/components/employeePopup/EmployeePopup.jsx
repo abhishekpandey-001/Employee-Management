ebf393ef2ba58482e213fa1fe017edf1e51353a3
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2026-01-01 23:19:29 +0530

    finished states

--- src/components/employeePopup/EmployeePopup.jsx
+++ src/components/employeePopup/EmployeePopup.jsx
@@ -1,7 +1,7 @@
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { closeEmployeePopup } from '../../store/features/popup/popup.slice';
-import { useState } from 'react';
-import { postEmployee } from '../../store/features/employee/employee.thunk';
+import { useEffect, useState } from 'react';
+import { postEmployee, updateEmployee } from '../../store/features/employee/employee.thunk';
 
 const EmployeePopup = () => {
     const [formDetails, setFormDetails] = useState({
@@ -11,43 +11,73 @@ const EmployeePopup = () => {
         email: '',
         highlight: false
     })
-    
 
-    const popup = useSelector(state=>state.popup.employeePopup);
+
+    const popup = useSelector(state => state.popup.employeePopup);
     const dispatch = useDispatch()
 
-    if(!popup) return null;
-
-    const handleInputChange = (e)=>{
-        const {name, value} = e.target;
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
         setFormDetails({
             ...formDetails,
             [name]: value
         })
     }
 
-    const handleSubmit = async ()=>{
-        await dispatch(postEmployee(formDetails));
+    const handleSubmit = async () => {
+        if (popup.id) {
+            await dispatch(updateEmployee({
+                id: popup.id,
+                details: formDetails
+            }))
+        }
+        else {
+            await dispatch(postEmployee(formDetails));
+        }
         dispatch(closeEmployeePopup());
     }
 
+    useEffect(() => {
+
+        if (popup) {
+            setFormDetails({
+                profileUrl: '',
+                bio: '',
+                name: '',
+                email: '',
+                highlight: false
+            })
+        }
+
+        else if (popup?.id) {
+            setFormDetails({
+                profileUrl: popup.profileUrl,
+                bio: popup.bio,
+                name: popup.name,
+                email: popup.email,
+                highlight: false
+            })
+        }
+    }, [popup])
+
+    if (!popup) return null;
 
     return (
-        <div className='fixed top-0 left-0 w-full h-full bg-black/80 z-20 flex items-center justify-center' onClick={()=>dispatch(closeEmployeePopup())}>
-            <fieldset className="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-4" onClick={(e)=>e.stopPropagation()}>
+        <div className='fixed top-0 left-0 w-full h-full bg-black/80 z-20 flex items-center justify-center' onClick={() => dispatch(closeEmployeePopup())}>
+            <fieldset className="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-4" onClick={(e) => e.stopPropagation()}>
                 <legend className="fieldset-legend">Employee Detaiils</legend>
 
                 <label className="label">Profile URL</label>
-                <input type="text" className="input" placeholder="Profile URL" name='profileUrl' onChange={handleInputChange}/>
+                <input type="text" className="input" placeholder="Profile URL" name='profileUrl' onChange={handleInputChange} value={formDetails.profileUrl} />
 
                 <label className="label">Name</label>
-                <input type="text" className="input" placeholder="Name" name='name' onChange={handleInputChange}/>
+                <input type="text" className="input" placeholder="Name" name='name' onChange={handleInputChange} value={formDetails.name} />
 
                 <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" name='email' onChange={handleInputChange} />
+                <input type="email" className="input" placeholder="Email" name='email' onChange={handleInputChange} value={formDetails.email} />
 
                 <label className='label'>Bio</label>
-                <textarea className='textarea h-24' placeholder='Bio' name='bio' onChange={handleInputChange}></textarea>
+                <textarea className='textarea h-24' placeholder='Bio' name='bio' onChange={handleInputChange} value={formDetails.bio}></textarea>
 
                 <button onClick={handleSubmit} className='btn btn-neutral mt-4'>Submit</button>
             </fieldset>
